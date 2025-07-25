d865fac64220bee2a24387b6df8c8706016df21f
yhsunny176 <yeasinulhaq763@gmail.com>
2025-07-25 08:53:11 +0600

    Changed css styling of the Error page to be compatible to dark mode colors

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -3,14 +3,14 @@ import { Link } from "react-router";
 
 const Error = () => {
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+        <div className="min-h-screen flex flex-col items-center justify-center bg-background-primary">
             <div className="text-center">
-                <h1 className="text-9xl font-bold text-gray-800 mb-4">404</h1>
-                <h2 className="text-4xl font-semibold text-gray-700 mb-4">Page Not Found</h2>
-                <p className="text-xl text-gray-600 mb-8">Sorry, the page you are looking for doesn't exist.</p>
+                <h1 className="text-9xl font-bold text-black-text-dark mb-4">404</h1>
+                <h2 className="text-4xl font-semibold text-black-text-base mb-4">Page Not Found</h2>
+                <p className="text-xl text-black-text-base mb-8">Sorry, the page you are looking for doesn't exist.</p>
                 <Link
                     to="/"
-                    className="bg-blue-600 text-white px-6 py-3 rounded-lg text-lg font-medium hover:bg-blue-700 transition-colors">
+                    className="bg-red-base text-white-base px-6 py-3 rounded-lg text-lg font-medium hover:bg-red-dark transition-colors">
                     Go Back Home
                 </Link>
             </div>
